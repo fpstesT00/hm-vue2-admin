0e6c1ceffd575c6464273dae0748526adbf6ced5
Labmem Xie <141630413+fpstesT00@users.noreply.github.com>
2024-02-23 14:32:35 +0800

    调用用户资料存储vuex

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,9 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
-import { login } from '@/api/user'
+import { login, getUserInfo } from '@/api/user'
 // 存放数据
 const state = {
-  token: getToken()// 从缓存中读取初始值
+  token: getToken(), // 从缓存中读取初始值
+  userInfo: {}
 }
 
 // 修改数据
@@ -16,6 +17,9 @@ const mutations = {
     // 删除vuex的token
     state.token = null
     removeToken()
+  },
+  setUserInfo(state, userInfo) {
+    state.userInfo = userInfo
   }
 }
 
@@ -23,12 +27,15 @@ const mutations = {
 const actions = {
   // context上下文，传入参数
   async login(context, data) {
-    // console.log(data)
-    console.log(data)
     // todo: 调用登录接口
     const token = await login(data)
     // 返回一个token '123456'
     context.commit('setToken', token)
+  },
+  async getUserInfo(context) {
+    // console.log('测试')
+    const result = await getUserInfo()
+    context.commit('setUserInfo', result)
   }
 }
 
