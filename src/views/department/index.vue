c228a46d3e25bc4e335f6817f6dbbfc69d23a3bd
Labmem Xie <141630413+fpstesT00@users.noreply.github.com>
2024-02-25 21:29:52 +0800

    树形组件的运用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <el-tree default-expand-all :data="depts" :props="defaultProps" @node-click="handleNodeClick" />
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      depts: [{ name: '待定', children: [{
+        name: '总裁办'
+      }, {
+        name: '行政部'
+      }, {
+        name: '人事部'
+      }] }],
+      defaultProps: {
+        label: 'name', // 要显示的字段的名字
+        children: 'children'// 读取子节点的字段名
+      }
+    }
+  }
 }
 </script>
+<style scoped lang="scss">
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
+</style>
