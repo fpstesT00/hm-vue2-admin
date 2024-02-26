d577cbcb671a3d0e50b382fdcd908e04cf628fe5
Labmem Xie <141630413+fpstesT00@users.noreply.github.com>
2024-02-26 20:40:52 +0800

    新增部门弹层

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -2,9 +2,9 @@
   <div class="container">
     <div class="app-container">
       <!-- 展示树形结构 -->
-      <el-tree default-expand-all :data="depts" :props="defaultProps" @node-click="handleNodeClick">
+      <el-tree :expand-on-click-node="false" default-expand-all :data="depts" :props="defaultProps">
         <!-- 节点结构 -->
-        <!-- v-slot="{ node,data }" -->
+        <!-- v-slot="{ node, data }" 只能作用在template -->
         <template v-slot="{ data }">
           <el-row
             style="width: 100%; height: 40px"
@@ -14,56 +14,64 @@
           >
             <el-col>{{ data.name }}</el-col>
             <el-col :span="4">
-              <span class="tree-manager">{{ data.managerName }}</span>
-              <el-dropdown trigger="click">
-                <!-- 显示内容区域 -->
-                <span class="el-dropdown-link">
-                  操作
-                  <i class="el-icon-arrow-down el-icon--right" />
-                </span>
-                <!-- 下拉菜单 -->
-                <el-dropdown-menu slot="dropdown">
-                  <el-dropdown-item>添加子部门</el-dropdown-item>
-                  <el-dropdown-item>编辑部门</el-dropdown-item>
-                  <el-dropdown-item>删除</el-dropdown-item>
-                </el-dropdown-menu>
-              </el-dropdown>
+              <div class="drop-menu">
+                <span class="tree-manager">{{ data.managerName }}</span>
+                <!-- $event 实参 表示类型 -->
+                <el-dropdown trigger="click" @command="operateDept">
+                  <!-- 显示区域内容 -->
+                  <span class="el-dropdown-link">
+                    操作
+                    <i class="el-icon-arrow-down el-icon--right" />
+                  </span>
+                  <!-- 下拉菜单选项 -->
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                    <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                    <el-dropdown-item command="del">删除</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </div>
             </el-col>
           </el-row>
         </template>
       </el-tree>
-      <!-- 节点结构 -->
     </div>
+    <!-- 放置弹层 -->
+    <!-- 表示会接受子组件的事件 -->
+    <add-dept :show-dialog.sync="showDialog" />
   </div>
 </template>
 <script>
+import { getDepartment } from '@/api/department'
+import { transListToTreeData } from '@/utils'
+import AddDept from './components/app-dept.vue'
 export default {
   name: 'Department',
+  components: { AddDept },
   data() {
     return {
-      depts: [
-        {
-          name: '未来科技',
-          managerName: '管理员',
-          children: [
-            {
-              name: '总裁办',
-              managerName: '张三'
-            },
-            {
-              name: '行政部',
-              managerName: '李田所'
-            },
-            {
-              name: '人事部',
-              managerName: '王浩二'
-            }
-          ]
-        }
-      ],
+      showDialog: false,
+      depts: [], // 数据属性
       defaultProps: {
         label: 'name', // 要显示的字段的名字
         children: 'children' // 读取子节点的字段名
+      }
+    }
+  },
+  created() {
+    this.getDepartment() // 调用获取数据的接口
+  },
+  methods: {
+    // 封装好方法
+    async getDepartment() {
+      const result = await getDepartment()
+      this.depts = transListToTreeData(result, 0)
+    },
+    // 操作部门方法
+    operateDept(type) {
+      // 新增部门
+      if (type === 'add') {
+        this.showDialog = true
       }
     }
   }
@@ -74,9 +82,14 @@ export default {
   padding: 30px 140px;
   font-size: 14px;
 }
+.drop-menu {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .tree-manager {
   width: 50px;
   display: inline-block;
-  margin: 10px;
+  margin: 40px;
 }
 </style>
